203a99e72fd07de92d78eec1b8ca654eb264ee96
David Whipple <dwhipp88@gmail.com>
2023-01-17 12:04:05 -0700

    Refactored route, I suspect the bug is on the backend

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -40,8 +40,8 @@ export const getCurrentProfile = () => async (dispatch) => {
 export const getProfiles = () => async (dispatch) => {
   dispatch({ type: CLEAR_PROFILE });
   try {
-    const res = await axios.get(url + '/api/profile');
-    // const res = await axios.get('/api/profile');
+    // const res = await axios.get(url + '/api/profile');
+    const res = await axios.get('/api/profile');
 
     dispatch({
       type: GET_PROFILES,
