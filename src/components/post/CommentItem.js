aefe1eb097a086fa9483db7280236635219e2e58
David Whipple <dwhipp88@gmail.com>
2023-01-25 18:42:31 -0700

    removed unnecessary tags

--- src/components/post/CommentItem.js
+++ src/components/post/CommentItem.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -15,7 +15,11 @@ const CommentItem = ({
     <div class="post bg-white p-1 my-1">
       <div>
         <Link to={`/profile/${user}`}>
-          <img class="round-img" src={avatar} alt="profile image" />
+          <img
+            class="round-img"
+            src={avatar}
+            alt="users profile avatar"
+          />
           <h4>{name}</h4>
         </Link>
       </div>
