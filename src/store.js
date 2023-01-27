31c376b542e02a6a3f6050e4dcbc35fd524686dc
David Whipple <dwhipp88@gmail.com>
2023-01-26 17:33:54 -0700

    reset store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-// import { createStore, applyMiddleware } from 'redux';
-import { configureStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -8,7 +7,7 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = configureStore(
+const store = createStore(
   rootReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
